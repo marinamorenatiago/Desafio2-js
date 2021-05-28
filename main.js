21a29ff131e0e2ec649f5fad62bf2d3704c463e9
marinamorenatiago <morenatiago@gmail.com>
2021-05-28 15:05:21 -0300

    corrigi um erro e acrescentei o if do mes de aniversario

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-let idade = 18
+let idade = 21
 let humano = true
 let nome = "Marina"
 let sobrenome = "Tiago"
 
-if((idade == 18)) {
+if((idade >= 18)) {
     console.log("você é maior de idade")
 }
 
-if((idade == 18) && (humano == true)) {
-    console.log("deu certo")
+if((idade >= 18) && (humano == true)) {
+    console.log("Você é maior de idade e é humano.")
 }
 
-if(nome.substring(0) == "R") {
+let aniversario = "Setembro"
+
+if(`(aniversario == "Janeiro") || (aniversario == "Dezembro")`) {
+    console.log("Você faz aniversário no verão.")
+}
+
+if(nome.substring(0,1) == "R") {
     console.log("Seu nome começa com a letra R")
 }
 else {
     console.log("Seu nome não começa com a letra R")
 }
 
-if(sobrenome.length > 6 || nome.substring(0) == "E") {
+if(sobrenome.length > 6 || nome.substring(0,1) == "E") {
     console.log("Você atende um ou todos os critérios")
 }
 else {
